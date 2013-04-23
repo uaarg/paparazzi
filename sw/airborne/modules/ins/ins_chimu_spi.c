3888f50ca83bb23cab880679580b2e426186fdf0
uaarg <uaarg@uaarg-autopilot.(none)>
2013-04-22 18:49:23 -0600

    Adjusted values in airframe file based on flights. Added INS_PITCH_NEUTRAL_DEFAULT and INS_ROLL_NEUTRAL_DEFAULT suppport to the CHIMU driver.

--- sw/airborne/modules/ins/ins_chimu_spi.c
+++ sw/airborne/modules/ins/ins_chimu_spi.c
@@ -80,7 +80,7 @@ void parse_ins_msg( void )
           CHIMU_DATA.m_attitude.euler.phi -= 2 * M_PI;
         }
 
-        EstimatorSetAtt(CHIMU_DATA.m_attitude.euler.phi, CHIMU_DATA.m_attitude.euler.psi, CHIMU_DATA.m_attitude.euler.theta);
+        EstimatorSetAtt(CHIMU_DATA.m_attitude.euler.phi + INS_ROLL_NEUTRAL_DEFAULT, CHIMU_DATA.m_attitude.euler.psi, CHIMU_DATA.m_attitude.euler.theta + INS_PITCH_NEUTRAL_DEFAULT);
         EstimatorSetRate(CHIMU_DATA.m_sensor.rate[0],CHIMU_DATA.m_attrates.euler.theta,0.); // FIXME rate r
       }
       else if(CHIMU_DATA.m_MsgID==0x02) {
