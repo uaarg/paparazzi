7c064696d1bba5714fe839fcdd2c58f06472caea
Rijesh Augustine <rijesh@ualberta.ca>
2015-10-21 17:53:27 -0600

    magnetometer is fully aligned and tested. Gyro and Accelerometers are aligned using UAARG_MOTHRA flag rather than BODY to IMU. Need to calibrate GYRO neutrals, and MAGNETOMETER

--- sw/airborne/subsystems/imu/imu_aspirin_2_spi.c
+++ sw/airborne/subsystems/imu/imu_aspirin_2_spi.c
@@ -232,6 +232,22 @@ void imu_aspirin2_event(void)
     /* Handle axis assignement for Lisa/M or Lisa/MX V2.1 integrated Aspirin like
      * IMU.
      */
+
+#ifdef UAARG_MOTHRA
+    RATES_ASSIGN(imu.gyro_unscaled,
+                 imu_aspirin2.mpu.data_rates.rates.p,
+                 imu_aspirin2.mpu.data_rates.rates.r,
+                 -imu_aspirin2.mpu.data_rates.rates.q);
+    VECT3_ASSIGN(imu.accel_unscaled,
+		 imu_aspirin2.mpu.data_accel.vect.x,
+                 imu_aspirin2.mpu.data_accel.vect.z,
+                 -imu_aspirin2.mpu.data_accel.vect.y);
+#if !ASPIRIN_2_DISABLE_MAG
+    VECT3_ASSIGN(imu.mag_unscaled, -mag.x, -mag.y, mag.z);
+#endif
+#else
+
+
 #ifdef LISA_M_OR_MX_21
     RATES_ASSIGN(imu.gyro_unscaled,
                  -imu_aspirin2.mpu.data_rates.rates.q,
