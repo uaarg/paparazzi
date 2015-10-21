d9eecea03b8203cca07257017fffbb7edd87f814
Rijesh Augustine <rijesh@ualberta.ca>
2015-10-21 17:53:27 -0600

    magnetometer is fully aligned and tested. Gyro and Accelerometers are aligned using UAARG_MOTHRA flag rather than BODY to IMU. Need to calibrate GYRO neutrals, and MAGNETOMETER

--- sw/airborne/modules/sensors/mag_hmc58xx.c
+++ sw/airborne/modules/sensors/mag_hmc58xx.c
@@ -94,7 +94,7 @@ void mag_hmc58xx_module_event(void)
 
     // set channel order
     struct Int32Vect3 mag = {
-      (int32_t)(-mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
+      (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
       (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Y]),
       (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
     };
@@ -126,7 +126,7 @@ void mag_hmc58xx_module_event(void)
 void mag_hmc58xx_report(void)
 {
   struct Int32Vect3 mag = {
-    (int32_t)(-mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
+    (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
     (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Y]),
     (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
   };
