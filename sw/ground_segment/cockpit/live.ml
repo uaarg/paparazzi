6d4a55e52bd6869d81f58a3e45401e2bbcf61f27
Rijesh Augustine <rijesh@ualberta.ca>
2015-11-09 18:48:13 -0700

    First Obstacle Addition Commit

--- sw/ground_segment/cockpit/live.ml
+++ sw/ground_segment/cockpit/live.ml
@@ -1460,7 +1460,7 @@ let listen_tcas = fun a timestamp ->
   tele_bind "TCAS_TA" (get_alarm_tcas a "tcas TA") timestamp;
   tele_bind "TCAS_RA" (get_alarm_tcas a "TCAS RA") timestamp
 
-let get_intruders = fun (geomap:G.widget) _sender vs ->
+let get_intruders = fun  (geomap:G.widget)_sender vs ->
   let f = fun s -> Pprz.float_assoc s vs in
   let i = fun s -> float (Pprz.int_assoc s vs) in
   let name = Pprz.string_assoc "name" vs
@@ -1469,13 +1469,42 @@ let get_intruders = fun (geomap:G.widget) _sender vs ->
   and lat = (i "lat") /. 1e7
   and lon = (i "lon") /. 1e7 in
   let pos = { posn_lat=(Deg>>Rad)lat; posn_long=(Deg>>Rad)lon } in
-  if not (Intruders.intruder_exist id) then
-    Intruders.new_intruder id name time geomap;
+  if not (Intruders.intruder_exist id) then begin
+    Intruders.new_intruder id name time geomap
+  end;
   Intruders.update_intruder id pos (f "course") ((i "alt") /. 1000.) (f "speed") (f "climb") time
 
 let listen_intruders = fun (geomap:G.widget) ->
   safe_bind "INTRUDER" (get_intruders geomap)
 
+let get_obstacles = fun  a (geomap:G.widget)_sender vs ->
+  let f = fun s -> Pprz.float_assoc s vs in
+  let i = fun s -> float (Pprz.int_assoc s vs) in
+  let id = Pprz.string_assoc "id" vs
+  and shape = Pprz.string_assoc "shape" vs
+  and status = i "status"
+  and lat = (i "lat") /. 1e7
+  and lon = (i "lon") /. 1e7
+  and time = Unix.gettimeofday () in
+  let pos = { posn_lat=(Deg>>Rad)lat; posn_long=(Deg>>Rad)lon } in
+  if not (Obstacles.obstacle_exist id) then begin
+    log ~say:true a "5" "Generating New Obstacle";
+    Obstacles.new_obstacle id shape (f "radius") time geomap;
+    Obstacles.update_obstacle id pos ((i "alt") /. 1000.) time;
+  end;
+  if status==1 then
+  log ~say:true a "5" "Updating Moving Obstacle";
+  Obstacles.update_obstacle id pos ((i "alt") /. 1000.) time;
+  if status==3 then
+  log ~say:true a "5" "Deleting Obstacle";
+  Obstacles.delete_obstacle id
+
+
+let listen_obstacles = fun (geomap:G.widget) a ->
+  safe_bind "OBSTACLE" (get_obstacles a geomap)
+
+let print_rijesh = fun a astring ->
+  log ~say:true a "5" astring
 
 let listen_acs_and_msgs = fun geomap ac_notebook strips my_alert auto_center_new_ac alt_graph timestamp ->
   (** Probe live A/Cs *)
@@ -1502,6 +1531,8 @@ let listen_acs_and_msgs = fun geomap ac_notebook strips my_alert auto_center_new
   listen_tcas my_alert timestamp;
   listen_dcshot geomap timestamp;
   listen_intruders geomap;
+  listen_obstacles geomap my_alert;
+  print_rijesh my_alert "hi how ware you";
 
   (** Select the active aircraft on notebook page selection *)
   let callback = fun i ->
