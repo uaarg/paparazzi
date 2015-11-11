b44eb425bb98172459d9c05f1c83bfd6c936f1ea
Rijesh Augustine <rijesh@ualberta.ca>
2015-11-10 20:02:47 -0700

    Obstacle placement comeplete. Need to work on obj type/alt label

--- sw/ground_segment/cockpit/obstacles.ml
+++ sw/ground_segment/cockpit/obstacles.ml
@@ -19,60 +19,39 @@
  *
  *)
 
-(*open Latlong*)
-
 open Printf
-module G2d = Geometry_2d
-module LL = Latlong
 
 module G = MapCanvas
 
-module CL = ContrastLabel
-module ACI = AcIcon
-
-type desired =
-    NoDesired
-  | DesiredCircle of LL.geographic*float*GnoCanvas.ellipse
-  | DesiredSegment of LL.geographic*LL.geographic*GnoCanvas.line
-
-
-
-type obstacle = {
-  obstacle_track : MapTrack.track;
+type obstacles = {
+  obsid : string;
+  mutable circleshape : GnoCanvas.ellipse;
   mutable last_update : float
 }
 
 
-(*let obstacles = (string, obstacle) Hashtbl.t*)
 let obstacles = Hashtbl.create 1
 
-let new_obstacle = fun id name radius time geomap ->
-  let track = new MapTrack.track ~size:200 ~icon:"fixedwing" ~name ~show_carrot:false id geomap in
-  let obstacle = { obstacle_track = track; last_update = time } in
+let new_obstacle = fun id wgs84 fillcolor radius time (geomap:MapCanvas.widget) ->
+  let gencircle = geomap#circle ~fill_color:fillcolor  ~color:fillcolor wgs84 radius in
+  let obstacle = {obsid = id; circleshape = gencircle; last_update = time } in 
   Hashtbl.add obstacles id obstacle
 
 let remove_obstacle = fun id ->
   try
-    let obstacle = Hashtbl.find obstacles id in
-    obstacle.obstacle_track#destroy ();
-    Hashtbl.remove obstacles id
-  with _ -> () (* no obstacle *)
+   let obstacle = Hashtbl.find obstacles id in
+   obstacle.circleshape#destroy ();
+   Hashtbl.remove obstacles id
+  with _ -> ()
 
-let update_obstacle = fun id wgs84 alt radius time (geomap:MapCanvas.widget) ->
+let update_obstacle = fun id wgs84 fillcolor radius time (geomap:MapCanvas.widget) ->
   try
     let obstacle = Hashtbl.find obstacles id in
-    obstacle.obstacle_track#move_icon wgs84 0.0 alt 0.0 0.0;
-    DesiredCircle (wgs84, radius, geomap#circle ~color:"#00ff00" wgs84 radius);
-    obstacle.last_update <- time;
-  with _ -> () (* no obstacle, add a new one ? *)
-
-(*  let draw_obstacle = fun en radius ->
-    let create = fun () ->
-      desired_track <- DesiredCircle (en, radius, geomap#circle ~color:"#00ff00" en radius) in
-    create()
-*)
-
+    let gencircle = geomap#circle ~fill_color:fillcolor  ~color:fillcolor wgs84 radius in
+    obstacle.circleshape#destroy ();
+    obstacle.circleshape <- gencircle;
+    obstacle.last_update <- time
+  with _ -> () 
 
 let obstacle_exist = fun id ->
   Hashtbl.mem obstacles id
-
