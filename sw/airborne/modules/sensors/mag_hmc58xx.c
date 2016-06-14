e37fd5958b95c0a47dab7ff9401c9b2bb07030a5
uaarg <rijesh@ualberta.ca>
2016-06-13 19:22:04 -0600

    pre us comp pretty paparazzi working tree

--- sw/airborne/modules/sensors/mag_hmc58xx.c
+++ sw/airborne/modules/sensors/mag_hmc58xx.c
@@ -96,7 +96,7 @@ void mag_hmc58xx_module_event(void)
     struct Int32Vect3 mag = {
       (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
       (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Y]),
-      (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
+      (int32_t)(-mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
     };
     // only rotate if needed
 #if USE_MAG_TO_IMU
@@ -128,7 +128,7 @@ void mag_hmc58xx_report(void)
   struct Int32Vect3 mag = {
     (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_X]),
     (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Y]),
-    (int32_t)(mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
+    (int32_t)(-mag_hmc58xx.data.value[HMC58XX_CHAN_Z])
   };
   DOWNLINK_SEND_IMU_MAG_RAW(DefaultChannel, DefaultDevice, &mag.x, &mag.y, &mag.z);
 }
