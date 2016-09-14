810d5273db8c1ee7dca6a5ea8b902e209fe60492
uaarg <rijesh@ualberta.ca>
2016-09-14 13:17:54 -0600

    added new flight configurations

--- sw/airborne/subsystems/imu/imu_aspirin_2_spi.c
+++ sw/airborne/subsystems/imu/imu_aspirin_2_spi.c
@@ -228,24 +228,6 @@ void imu_aspirin2_event(void)
     VECT3_ASSIGN(imu.mag_unscaled, -mag.x, -mag.y, mag.z);
 #endif
 #else
-
-    /* Handle axis assignement for Lisa/M or Lisa/MX V2.1 integrated Aspirin like
-     * IMU.
-     */
-#ifdef UAARG_MOTHRA
-    RATES_ASSIGN(imu.gyro_unscaled,
-                 imu_aspirin2.mpu.data_rates.rates.p,
-                 imu_aspirin2.mpu.data_rates.rates.r,
-                 -imu_aspirin2.mpu.data_rates.rates.q);
-    VECT3_ASSIGN(imu.accel_unscaled,
-		 imu_aspirin2.mpu.data_accel.vect.x,
-                 imu_aspirin2.mpu.data_accel.vect.z,
-                 -imu_aspirin2.mpu.data_accel.vect.y);
-#if !ASPIRIN_2_DISABLE_MAG
-    VECT3_ASSIGN(imu.mag_unscaled, -mag.x, -mag.y, mag.z);
-#endif
-#else
-
     /* Handle axis assignement for Lisa/M or Lisa/MX V2.1 integrated Aspirin like
      * IMU.
      */
